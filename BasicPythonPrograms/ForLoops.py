e806d51442eb7b0f0538978af008e64735c531a4
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-08-07 20:17:17 +0530

    For loop
    Demonstration of for loop . like nesnted loop and pass statement

---
+++ BasicPythonPrograms/ForLoops.py
@@ -0,0 +1,9 @@
+#nested loops 
+adj=["red","big","tasty"]
+fruits=["apple","banana","cherry"]
+for x in adj:
+    for y in fruits:
+        print(x,y)
+#Pass statement
+for x in [0,2,3]:
+    pass        
