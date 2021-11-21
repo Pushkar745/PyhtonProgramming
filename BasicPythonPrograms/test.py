6464dd33661b77776001a195c61d88916f6cf8ee
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-11-21 09:47:35 +0530

    2pc
    Some Changes

--- BasicPythonPrograms/test.py
+++ BasicPythonPrograms/test.py
@@ -1,5 +1,4 @@
-thisset={1,5,8,7,6,7}
-print(thisset)
-print(type(thisset))
-for x in thisset:
-    print(x/2)
+if __name__ == '__main__':
+    n = int(input())
+    for x in range(1,n):
+     print(x,end='')
