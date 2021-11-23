92e93ec076f2b7514a4c2bfb0ad3056d7c7b852c
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-11-23 08:42:00 +0530

    Update Pythonexe12
    Calculate income tax for the given income by adhering to the below rules

---
+++ BasicPythonPrograms/pythonExe12.py
@@ -0,0 +1,14 @@
+#Calculate income tax for the given income by adhering to the below rules 
+income =5000
+tax_payable=0
+print("Given income ",income)
+if income <=10000:
+    tax_payable=0
+elif income <=20000:
+    x=income -10000
+    tax_payable=x * 10 /100
+else:
+    tax_payable=0
+    tax_payable=10000*10/100
+    tax_payable+=(income-20000) * 20 /100
+print("Toatl tax to pay is",tax_payable )    
