15c20b72a0434e723886f6dfe44490527bc5329f
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-11-24 17:04:43 +0530

    Update test.py
    test Some programs

--- BasicPythonPrograms/test.py
+++ BasicPythonPrograms/test.py
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
-if __name__ == '__main__':
-    n = int(input())
-    for x in range(1,n):
-     print(x,end='')
-=======
-import sys
-a=10
-b=10 
-print(id(a))
-print(id(b))
-c=20
-d=20
-e=20
-print(id(c))
-print(id(d))
-print(id(e))
-print(sys.getrefcount(a))
-print(sys.getrefcount(c))
->>>>>>> a9ec4dae7c3269e11808a40e56a50235c82881a0
+import random
+a=range(1,20)
+print(random.randrange(1,20))
+for i in range(1,20):
+    print(i) 
+    for j in a:
+        print(i+j,end=", ")
