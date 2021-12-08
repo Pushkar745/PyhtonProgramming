9cfd4748d5b6929f682c70643bba802078a43a45
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-12-08 21:10:01 +0530

    create NOtInOperator
    illustration of not in operator

---
+++ BasicPythonPrograms/NotInOperator.py
@@ -0,0 +1,11 @@
+x=24
+y=40
+list=[10,20,30,40,50];
+if (x not in list):
+    print("X is not present in given list")
+else:
+    print("x is present in given list")
+if (y in list):
+    print("y is present in given list")
+else:
+    print("y is not present in given list")           
