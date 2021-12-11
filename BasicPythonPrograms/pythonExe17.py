ddbbea598b712dc16bdb8864e8cf8e0cb73a0db5
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-12-11 20:53:24 +0530

    Create PythonEXE17
    Illustration of Nested If Statement

---
+++ BasicPythonPrograms/pythonExe17.py
@@ -0,0 +1,12 @@
+i =10
+if(i==10):
+    if(i<15):
+        print("I is Smaller then 15")
+
+        #Nested if statement
+
+    if(i<12):
+        print("Is is smaller then 12 too")
+    else:
+        print("i is greater then 15")
+                
