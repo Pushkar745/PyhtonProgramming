44e59251dbc26c7b514e8aa8fcfcc42340af2758
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2021-12-15 20:24:20 +0530

    Create PythonConstrutor
    illustration of Constructor

---
+++ BasicPythonPrograms/PythonConstrutor.py
@@ -0,0 +1,11 @@
+class Geek:
+    #Default Constructor
+    def __init__(self):
+        self.geek="I am Geeek"
+        # a Method for printing data members
+        def print_Geek(self):
+            print(self.geek)
+#Creating object
+obj=Geek()
+#Calling the instance method using the object
+obj.print_Geek()
