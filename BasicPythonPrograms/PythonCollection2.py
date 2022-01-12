6398dba9d7009fbe00b35a933727ff4cdb45af07
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2022-01-12 16:23:18 +0530

    update PythonCollection
    A Python program to demonstrate working of OrderedDict.

--- BasicPythonPrograms/PythonCollection2.py
+++ BasicPythonPrograms/PythonCollection2.py
@@ -1,1 +1,18 @@
-# A
+# A Python program to demonstrate working of orderdDict
+from collections import OrderedDict
+od=OrderedDict()
+od['a']=1
+od['b']=2
+od['c']=3
+od['d']=4
+print('Before Deleting ')
+for key,value in od.items():
+    print(key,value)
+#Deleting element
+od.pop('a')
+#Re-inserting the same
+od['a']=1
+print('\nAfter re-inserting')
+for key,value in od.items():
+    print(key,value)
+
