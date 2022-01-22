87a90dc23c39f8086c3a2dda6dea8784b244a5e6
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2022-01-22 17:46:33 +0530

    Create PythonCounters
    A Python program to show different ways to create Counter.

---
+++ BasicPythonPrograms/PythonCounters.py
@@ -0,0 +1,8 @@
+#A Python program to show different ways to create Counter
+from collections import Counter
+#With sequence of items 
+print(Counter(['B','B','A','B','C','A','B','B','A','C']))
+#with Dictionary
+print(Counter({'A':3,'B':5,'C':2}))
+#With keyword arguments
+print(Counter(A=3,B=5,c=2))
