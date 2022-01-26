9b4743144191245305a0b8003084210308277c07
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2022-01-26 19:11:46 +0530

    Create PythonOrderedDict
    A Python program to demonstrate working of OrderedDict .

---
+++ BasicPythonPrograms/PythonOrderedDict.py
@@ -0,0 +1,18 @@
+#A Python program to demonstrate working of OrderedDict 
+from collections import OrderedDict
+print("This is a Dict :\n")
+d={}
+d['a']=1
+d['b']=4
+d['c']=2
+d['d']=3
+for key,value in d.items():
+    print(key,value)
+print("\nThis is an Ordered Dict:\n")
+od=OrderedDict()
+od['a']=1
+od['b']=2
+od['c']=3
+od['d']=4
+for key, value in od.items():
+    print(key,value)
