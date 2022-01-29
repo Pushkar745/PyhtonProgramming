58a323fd7fa2f5b774ed81ed3452117a915accda
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2022-01-29 12:55:32 +0530

    Create PythonDefaultdict
    Dictionary in Python is an unordered collection of data values that are used to store data values like a map.

---
+++ BasicPythonPrograms/PythonDefaultdict.py
@@ -0,0 +1,8 @@
+#A Python program to demonstrate dictionary
+Dict={1:'a',2:'b',3:'c'}
+print("Dictionary:")
+print(Dict)
+print(Dict[1])
+#Uncommenting this print(Dict[4])
+#will raise a keyError as the
+#4 is not present in the dictionary
