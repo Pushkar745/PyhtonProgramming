df65e04ef58c9d9c786614d8e5e67ed480eba8c2
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2023-01-17 19:56:22 +0530

    Some changes
    Just some changes

--- BasicPythonPrograms/JoinList.py
+++ BasicPythonPrograms/JoinList.py
@@ -1,11 +1,11 @@
-list1=["a","b","c"]
-list2=[1,2,3]
-list3=list1+list2
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+list3 = list1+list2
 print(list3)
-#Another way to join two list is by appending all the items from list 2 in list 1
+# Another way to join two list is by appending all the items from list 2 in list 1
 for x in list2:
     list1.append(x)
-print(list1)    
-#another method is extend()
+print(list1)
+# another method is extend()
 list1.extend(list2)
 print(list1)
